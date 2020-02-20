67cb4825e68d58e9ac26242e2c859a0211fc9341
Kshitij Saxena <kshitj.saxena@mecuris.com>
2020-02-20 09:56:25 +0100

    Adding documentation to views and utils

--- currency/utils.py
+++ currency/utils.py
@@ -2,6 +2,14 @@ import requests
 
 
 def get_conversion(base, query, amount):
+    """
+    Call exchange rate API and get conversion
+
+    :param base: currency to convert from
+    :param query: currency to convert to
+    :param amount: amount to convert
+    :return:
+    """
     url = f"https://api.exchangeratesapi.io/latest?base={base}"
     response = requests.get(url)
     if response.status_code == 200:
--- currency/views.py
+++ currency/views.py
@@ -9,9 +9,17 @@ from .utils import get_conversion
 
 class CurrencyConvert(View):
     def get(self, request):
+        """
+        The view that renders HTML page to convert currencies
+        :param request: The request contains of params: base, query and amount
+        :return: HTML template running with Jinja Engine
+        """
         try:
+            # base: currency to convert from
             base_currency = request.GET['base'].upper()
+            # query: currency to convert to
             query_currency = request.GET['query'].upper()
+            # amount: amount to be converted
             query_amount = request.GET['amount']
             if base_currency not in CURRENCY_CHOICES or query_currency not in CURRENCY_CHOICES:
                 raise ValueError('Invalid currency choice')
@@ -25,7 +33,8 @@ class CurrencyConvert(View):
                 "query": query_currency,
                 "result": result_amount
             })
-        except KeyError as ke:
+        except KeyError:
+            # In case the user sends a bad request, i.e. without the params, redirect him to a default page!
             return redirect(f"/currency/convert?base={CURRENCY_CHOICES[0]}&query={CURRENCY_CHOICES[0]}&amount=0.0")
         except Exception as e:
             return HttpResponse(str(e))
